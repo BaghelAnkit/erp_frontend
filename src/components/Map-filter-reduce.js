66fa940d04ddbfa3da3a1fad97c9d32c751af485
anubhav singh <ankit.062.singh@gmail.com>
2025-11-29 10:44:29 +0530

    my code

---
+++ src/components/Map-filter-reduce.js
@@ -0,0 +1,13 @@
+// they are higher order function
+ import React from 'react'
+ 
+ function MapFilterReduce() {
+   return (
+     <div>
+       hello
+     </div>
+   )
+ }
+ 
+ export default MapFilterReduce
+ 
